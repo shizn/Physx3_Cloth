5317a95b6dd20ecfb89132c1ef90281b7042fb5b
Shin Shi <shizn@foxmail.com>
2015-04-12 23:16:26 +0800

    CCD done(whatever...) 10drops fall down to a piece of static cloth.

--- px3_fluid_absorb/main.cpp
+++ px3_fluid_absorb/main.cpp
@@ -56,7 +56,7 @@ Please refer to last chapter (PhysX Visual Debugger) for more information.
 using namespace std;
 using namespace physx;
 
-#define  FLUID_PARTICLE_NUMS 800
+#define  FLUID_PARTICLE_NUMS 20
 //--------------Global variables--------------//
 static PxPhysics*				gPhysicsSDK = NULL;			//Instance of PhysX SDK
 static PxFoundation*			gFoundation = NULL;			//Instance of singleton foundation SDK class
@@ -74,8 +74,13 @@ PxParticleFluid					*gFluid = NULL;				//Instance of Physx SPH Particle system
 //Fluid Data Buffer
 std::vector<PxU32> mTmpIndexArray;
 std::vector<PxVec3> positions;
+std::vector<PxVec3> positionsPrevious;
 std::vector<PxVec3> velocities;
+std::vector<PxVec3> forces;
+std::vector<PxVec3> impulses;
 
+PxU32* triangles;
+PxVec3 *clothParticlePos;
 //ContactInfos
 
 
@@ -95,7 +100,7 @@ void ShutdownPhysX();	//Shutdown PhysX SDK
 void ConnectPVD();		//Function for the visualization of PhysX simulation (Optional and 'Debug' mode only) 
 
 
-
+int DetectContactsBetweenParticleAndMesh(PxVec3 posParticle, PxVec3 velParticle, PxVec3 meshP0, PxVec3 meshP1, PxVec3 meshP2, float& u, float& v, float& w, PxVec3& n);
 
 
 void main()
@@ -162,10 +167,10 @@ void InitPhysX()
 	//---------Creating actors-----------]
 
 	//1-Creating static plane	 
-	PxTransform planePos = PxTransform(PxVec3(0.0f), PxQuat(PxHalfPi, PxVec3(0.0f, 0.0f, 1.0f)));	//Position and orientation(transform) for plane actor  
-	PxRigidStatic* plane = gPhysicsSDK->createRigidStatic(planePos);								//Creating rigid static actor	
-	plane->createShape(PxPlaneGeometry(), *material);												//Defining geometry for plane actor
-	gScene->addActor(*plane);																		//Adding plane actor to PhysX scene
+	//PxTransform planePos = PxTransform(PxVec3(0.0f), PxQuat(PxHalfPi, PxVec3(0.0f, 0.0f, 1.0f)));	//Position and orientation(transform) for plane actor  
+	//PxRigidStatic* plane = gPhysicsSDK->createRigidStatic(planePos);								//Creating rigid static actor	
+	//plane->createShape(PxPlaneGeometry(), *material);												//Defining geometry for plane actor
+	//gScene->addActor(*plane);																		//Adding plane actor to PhysX scene
 
 
 	//2-Creating dynamic cube																		 
@@ -195,11 +200,13 @@ void InitPhysX()
     //////////////////////////////////////////////////////////////////////////
     ///4.5 - For Paper
     //需要拿到的信息包括流体粒子的质量，流体粒子的位置、速度
-    int particleNum = gFluid->getMaxParticles();        //粒子个数
-    PxReal particleMass = gFluid->getParticleMass();    //粒子质量
+    //int particleNum = gFluid->getMaxParticles();        //粒子个数
+    //PxReal particleMass = gFluid->getParticleMass();    //粒子质量 暂时没有用
+
+    // lock SDK buffers of *PxParticleSystem* ps for reading
+
+
     
-    cout << "particleNum:" << particleNum << endl;
-    cout << "particleMass:" << particleMass << endl;
 
 
     //粒子的位置速度和作用力需要在update的时候每次用Buffer读取
@@ -219,8 +226,27 @@ void InitPhysX()
 	//5-Creating Cloth
 	CreateCloth();
 
+    //PxMaterial* material = gPhysicsSDK->createMaterial(0.5, 0.5, 0.5); //Creating a PhysX material
+    PxRigidStatic *gParticle = NULL;
+    //PxTransform gParticlePos(PxVec3(-1.5,-1.29,0));
+    PxTransform gParticlePos(PxVec3(0, 10, 0));
+    PxSphereGeometry gParticleGeometry(0.03f);
+    PxShape* shape = gPhysicsSDK->createShape(gParticleGeometry, *material, false);
+
+    gParticle = PxCreateStatic(*gPhysicsSDK, gParticlePos, *shape);
+
+    gScene->addActor(*gParticle);
 
 
+    PxRigidStatic *gParticle1 = NULL;
+    //gParticle1 = PxCreateStatic(*gPhysicsSDK, PxTransform(PxVec3(-1.5, -0.99, -0.3)), *shape);
+    gParticle1 = PxCreateStatic(*gPhysicsSDK, PxTransform(PxVec3(0, 10, 10)), *shape);
+    gScene->addActor(*gParticle1);
+
+    PxRigidStatic *gParticle2 = NULL;
+    //gParticle2 = PxCreateStatic(*gPhysicsSDK, PxTransform(PxVec3(-1.19, -0.99, -0.3)), *shape);
+    gParticle2 = PxCreateStatic(*gPhysicsSDK, PxTransform(PxVec3(10, 10, 0)), *shape);
+    gScene->addActor(*gParticle2);
 	//6-Contact Modification
 	
 	
@@ -239,8 +265,8 @@ void CreateTestSphere(PxVec3 pos, PxReal radius)
 void CreateCloth()
 {
 
-	PxTransform gPose = PxTransform(PxVec3(-3, 3, 8));
-
+	//PxTransform gPose = PxTransform(PxVec3(-3, 3, 8));
+    PxTransform gPose = PxTransform(PxVec3(0, 0, 0));
 	// create regular mesh
 	PxU32 resolution = 50;
 	PxU32 numParticles = resolution*resolution;
@@ -263,7 +289,7 @@ void CreateCloth()
 	}
 	// 现在所有布料粒子的位置已经确定 在每个的pos里面
 	// create triangles
-	PxU32* triangles = new PxU32[3 * numTriangles];
+	triangles = new PxU32[3 * numTriangles];
 	PxU32* iIt = triangles;
 	for (PxU32 i = 0; i < resolution - 1; ++i)
 	{
@@ -297,7 +323,7 @@ void CreateCloth()
 	// cook fabric
 	PxClothFabric* fabric = PxClothFabricCreate(*gPhysicsSDK, meshDesc, PxVec3(0, 1, 0));
 
-	delete[] triangles;
+	//delete[] triangles;
 
 	// create cloth
 	gCloth = gPhysicsSDK->createCloth(gPose, *fabric, particles, PxClothFlags());
@@ -335,23 +361,6 @@ void CreateCloth()
 
 }
 
-//办法1是创建用来和流体碰撞的三角形们
-void CreateGhostTriangles(PxU32* triangles)
-{
-	PxRigidDynamic *atri = NULL;
-	//PxTriangleMeshGeometry trianglegeometry()
-}
-//办法2是创建用来和布料碰撞的球体们
-void CreateGhostSphere()
-{
-	//每个粒子的速度和位置已经保存在position和velocity里面
-	//这些碰撞粒子并不需要真的存在，只需要加入到布料的碰撞队列中即可
-	//这样可以避免粒子和碰撞形体冲突
-}
-
-void CreateClothFromObj()
-{
-}
 
 void CreateFluid()
 {
@@ -378,26 +387,34 @@ void CreateFluid()
 	gFluid->setSimulationFilterData(collisionGroupWaterfall);
 
 	srand(time(NULL));
-	PxVec3 globalPosition(0.0f, 15.0f, 0.0f);
-	for (int i = 0; i < 800; ++i)
+	PxVec3 globalPosition(1.0f, 10.0f, -10.0f);
+    for (int i = 0; i < FLUID_PARTICLE_NUMS; ++i)
 	{
 		mTmpIndexArray.push_back(i);
 		float num1, num2, num3;
-		num1 = (rand() % 100)*0.01;
-		num2 = (rand() % 100)*0.01;
-		num3 = (rand() % 100)*0.01;
+		//num1 = (rand() % 100)*0.01;
+		//num2 = (rand() % 100)*0.01;
+		//num3 = (rand() % 100)*0.01;
+        num2 = i / 3.f;
+        num1 = 0.f;
+        num3 = 0.f;
 		PxVec3 pos(num1, num2, num3);
 		pos = pos + globalPosition;
 		//PxVec3 vel(-num1, -num2, -num3);
 		PxVec3 vel(0, 0, 0);
 		positions.push_back(pos);
+        //初始化前一刻的位置
+        positionsPrevious.push_back(pos);
 		velocities.push_back(vel);
+        //这里初始化力冲量和速度为0
+        forces.push_back(vel);
+        impulses.push_back(vel);
 		//cout << num1 << num2 << num3 << endl;
 	}
 
 	PxParticleCreationData particleCreationData;
 
-	particleCreationData.numParticles = 800;
+    particleCreationData.numParticles = FLUID_PARTICLE_NUMS;
 	particleCreationData.indexBuffer = PxStrideIterator<const PxU32>(&mTmpIndexArray[0]);
 	particleCreationData.positionBuffer = PxStrideIterator<const PxVec3>(&positions[0]);
 	particleCreationData.velocityBuffer = PxStrideIterator<const PxVec3>(&velocities[0]);
@@ -415,6 +432,118 @@ void StepPhysX()					//Stepping PhysX
 {
 	gScene->simulate(gTimeStep);	//Advances the simulation by 'gTimeStep' time
 	gScene->fetchResults(true);		//Block until the simulation run is completed
+
+
+    //这里加入自己定义的碰撞检测的内容
+    //首先拿到各个粒子的参数
+    //int particleNum = gFluid->getMaxParticles();        //粒子个数
+    //PxReal particleMass = gFluid->getParticleMass();    //粒子质量
+
+    //接下来获取粒子运动时的速度和坐标
+    PxParticleReadData* rd = gFluid->lockParticleReadData();
+    
+    // access particle data from PxParticleReadData
+    if (rd)
+    {
+        PxStrideIterator<const PxParticleFlags> flagsIt(rd->flagsBuffer);
+        PxStrideIterator<const PxVec3> positionIt(rd->positionBuffer);
+        PxStrideIterator<const PxVec3> velocityIt(rd->velocityBuffer);
+        for (unsigned i = 0; i < rd->validParticleRange; ++i, ++flagsIt, ++positionIt, ++velocityIt)
+        {
+            if (*flagsIt & PxParticleFlag::eVALID)
+            {
+                // access particle position
+                const PxVec3& position = *positionIt;
+                const PxVec3& velocity = *velocityIt;
+                //更新此刻流体粒子的位置
+                positions[i] = position;
+                //cout << "pos:" << position.x << " " << position.y << " " << position.z << " " << endl;
+                //cout << "vel:" << velocity.x << " " << velocity.y << " " << velocity.z << " " << endl;
+
+                //接下来要根据已经获取的布料所有三角面片的坐标进行碰撞检测
+                //test case 1
+                //PxVec3 x1(0.f, 10.f, 0.f);
+                //PxVec3 x2(0.f, 10.f, 10.f);
+                //PxVec3 x3(10.f, 10.f, 0.f);
+                //这里获得布料的所有粒子位置
+                PxClothParticleData* data = gCloth->lockParticleData(PxDataAccessFlag::eREADABLE);
+                PxU32 clothParticleNum = gCloth->getNbParticles();
+                clothParticlePos = new PxVec3[clothParticleNum];
+                for (PxU32 j = 0; j < clothParticleNum; ++j)
+                {
+                    clothParticlePos[j] = data->particles[j].pos;            
+                }
+                //重组所有的三角面片
+                bool collisionDetected = false;
+                for (PxU32 j = 0; j < 14406; j=j+3)
+                {
+
+                    float u, v, w = 0.0f;
+                    PxVec3 n(0.f, 0.f, 0.f);
+                    
+                    PxVec3 &x3 = clothParticlePos[triangles[j]];
+                    PxVec3 &x2 = clothParticlePos[triangles[j + 1]];
+                    PxVec3 &x1 = clothParticlePos[triangles[j + 2]];
+                    if (DetectContactsBetweenParticleAndMesh(position, velocity, x1, x2, x3, u, v, w, n))
+                    {
+                        //cout << "这里碰撞了" << endl;
+
+                        //碰撞发生之后 需要往布料和流体粒子上作用一个冲量
+                        //根据Huber-2013 对于冲量的计算 可以加入一个布料的厚度来改进公式
+                        n.normalize();
+                        float d = 0.12 - (position - u*x1 - v*x2 - w*x3).dot(n);
+                        // 0.02这里表示一个临时的布料厚度
+                        // 接下来 I = -kd * Dt
+                        int particleNum = gFluid->getMaxParticles();        //粒子个数
+                        PxReal particleMass = gFluid->getParticleMass();    //粒子质量 暂时没有用
+                        //粒子的位置速度和作用力需要在update的时候每次用Buffer读取
+                        //需要更新的信息主要是冲量
+                        //impulses[i] = gCloth->getStretchConfig(PxClothFabricPhaseType::eBENDING).stiffness*d*gTimeStep*(-1)*n;
+                        
+                        impulses[i] = 10*gFluid->getParticleMass() * (velocity.magnitude()) * (n);
+                        //强制更新到前一刻的位置
+                        //强制考虑布料厚度问题
+                        positionsPrevious[i] = positionsPrevious[i] + (0.1f)*(n);
+                        //positionsPrevious[i].y += 0.1f;
+                        positions[i] = positionsPrevious[i];
+
+                        collisionDetected = true;
+                    }
+                    if (collisionDetected)
+                    {
+                        PxStrideIterator<const PxVec3> impluseBuffer(&impulses[0]);
+                        PxStrideIterator<const PxVec3> positionBuffer(&positions[0]);
+                        PxStrideIterator<const PxU32> indexBuffer(&mTmpIndexArray[0]);
+                        gFluid->addForces(gFluid->getMaxParticles(), indexBuffer, impluseBuffer, PxForceMode::eIMPULSE);
+                        rd->unlock();
+                        gFluid->setPositions(gFluid->getMaxParticles(), indexBuffer, positionBuffer);
+                        
+                        //OK!
+
+                        //布料这一块也需要更新冲量
+
+                        //NOK
+
+                        //更新一些临时变量
+                        collisionDetected = false;
+                        impulses[i] = PxVec3(PxZero);
+                    }
+
+                }
+                //释放内存
+                delete clothParticlePos;
+
+                //保存上一次计算后流体粒子的位置和速度
+                positionsPrevious[i] = position;
+
+            }
+
+        }
+
+        // return ownership of the buffers back to the SDK
+        rd->unlock();
+    }
+    //其次拿到规整的布料粒子的位置
 }
 
 
@@ -485,3 +614,64 @@ const char* findPath(const char* path)
 		return path;
 }
 
+
+//实时碰撞检测算法技术P132
+int DetectContactsBetweenParticleAndMesh(PxVec3 posParticle, PxVec3 velParticle, PxVec3 meshP0, PxVec3 meshP1, PxVec3 meshP2, float& u, float& v, float& w, PxVec3& n)
+{
+    //if (posParticle.y<=10.1)
+    //{
+    //    cout << endl;
+    //}
+    PxVec3 &a = meshP0;
+    PxVec3 &b = meshP1;
+    PxVec3 &c = meshP2;
+
+    PxVec3 &p = posParticle;
+    PxVec3 &q = posParticle + gTimeStep*velParticle*1.2;
+
+    PxVec3 ab = b - a;
+    PxVec3 ac = c - a;
+    PxVec3 qp = p - q;
+
+    n = ab.cross(ac);
+
+    float d = qp.dot(n);
+
+    if (d <= 0.0f)
+    {
+        return 0;
+    }
+
+
+    PxVec3 ap = p - a;
+    float t = ap.dot(n);
+    if (t < 0.0f)
+    {
+        return 0;
+    }
+    if (t > d)
+    {
+        return 0;
+    }
+    
+
+    PxVec3 e = qp.cross(ap);
+    v = ac.dot(e);
+    if (v<0.0f || v>d)
+    {
+        return 0;
+    }
+    w = -ab.dot(e);
+    if (w < 0.0f || d < (v + w))
+    {
+        return 0;
+    }
+
+    float odd = 1.0f / d;
+    t *= odd;
+    v *= odd;
+    w *= odd;
+    u = 1.0f - v - w;
+
+    return 1;
+}
